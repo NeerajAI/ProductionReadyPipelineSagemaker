2135abd2ce34aa16efa951ea94bbe1676c4bf022
NeerajAI <neerajyds@gmail.com>
2025-05-10 18:29:55 +0530

    This is testing website

--- test.py
+++ test.py
@@ -5,3 +5,6 @@ from src.ProdPipeline import logger
 logger.info("Welcome to our custom logging")
 logger.info("This is an info message")
 logger.info("Path to the files")
+
+for i in range(5):
+    logger.info(f"Logging message {i+1}")
